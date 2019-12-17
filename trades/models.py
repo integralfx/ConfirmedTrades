f87d005fac6b831002a5ce52262334571c5d5390
integralfx <jordansu83@hotmail.com>
2019-12-17 17:54:07 +1100

    Make links from comment IDs

--- trades/models.py
+++ trades/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+import praw
+
+def parse_praw_file():
+    with open('trades/praw.txt', 'r') as file:
+        lines = file.read().split('\n')
+        return praw.Reddit(client_id=lines[0], client_secret=lines[1], user_agent=lines[2])
+
+reddit = parse_praw_file()
+
 class Redditor(models.Model):
     username = models.CharField(max_length=20, unique=True)
 
@@ -12,3 +21,6 @@ class Trade(models.Model):
     user2 = models.ForeignKey(Redditor, related_name='trades2', on_delete=models.CASCADE)
     comment_id = models.CharField(max_length=7)
 
+    def get_comment_url(self):
+        comment = reddit.comment(self.comment_id)
+        return 'https://www.reddit.com' + comment.permalink
