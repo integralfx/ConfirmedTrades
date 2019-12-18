337d1721f966167977b65ebf6c4c9c6f3db9e91c
integralfx <jordansu83@hotmail.com>
2019-12-18 20:01:47 +1100

    Add search

--- confirmedtrades/urls.py
+++ confirmedtrades/urls.py
@@ -5,6 +5,7 @@ from trades import views
 
 urlpatterns = [
     path('', views.TradeListView.as_view(), name='home'),
+    path('search', views.TradeListView.as_view(), name='search'),
     path('trades/<username>/', views.trades, name='trades'),
     path('admin/', admin.site.urls),
 ]
--- trades/views.py
+++ trades/views.py
@@ -10,8 +10,20 @@ class TradeListView(ListView):
     paginate_by = 20
     ordering = ['user1']
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        return ctx
+
+    def get_queryset(self):
+        if 'q' in self.request.GET:
+            q = self.request.GET['q']
+            return Trade.objects.filter(user1__username__icontains=q)
+        else:
+            return super().get_queryset()
+
 
 def trades(req, username):
     redditor = get_object_or_404(Redditor, username=username)
     trades = Trade.objects.filter(user1=redditor).order_by('user2')
     return render(req, 'trades/trades.html', { 'redditor': redditor, 'trades': trades })
+
