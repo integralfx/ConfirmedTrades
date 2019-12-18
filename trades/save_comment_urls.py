4f4b60e92825ae814d5a4784914df5b83410f864
integralfx <jordansu83@hotmail.com>
2019-12-18 13:19:37 +1100

    Check if file exists

--- trades/save_comment_urls.py
+++ trades/save_comment_urls.py
@@ -1,9 +1,16 @@
 from trades.models import Redditor, Trade
 from tqdm import tqdm
+import os.path
 
 def save_comment_urls(start_id, count=1000):
     trades = Trade.objects.filter(id__gte=start_id)[:count]
-    with open(f'url_{start_id}-{start_id + count - 1}.txt', 'w') as file:
+    filename = f'url_{start_id}-{start_id + count - 1}.txt'
+
+    if os.path.isfile(filename):
+        print(f'{filename} already exists')
+        return
+
+    with open(filename, 'w') as file:
         with tqdm(total=trades.count(), unit='trades') as pbar:
             for trade in trades:
                 comment_url = trade.get_comment_url()
