f86a5290cfc3f8429da6ef95c42df575ae7ee02c
integralfx <jordansu83@hotmail.com>
2019-12-22 22:20:48 +1100

    Quality of life sorting improvement

--- trades/templatetags/query_transform.py
+++ trades/templatetags/query_transform.py
@@ -16,13 +16,23 @@ def query_transform(request, **kwargs):
 def sort_query(request, param, delete_others):
     params = request.GET.copy()
 
-    if delete_others:
-        params = { k: v for k, v in params.items() if not k.startswith('sort') or k == param }
-
     if param in params:
         params[param] = 'asc' if params[param] == 'desc' else 'desc'
     else:
-        params[param] = 'desc'
+        # Initially, the home page is already sorted by the user but doesn't have the 'sort-user=asc' query. Clicking on
+        # it in this case, should sort the users in descending order.
+        # However, changing the sorted column should sort in ascending order. For example, say the get query is 
+        # 'sort-trades=asc'. Clicking on the user column would result in 'sort-user=asc'.
+        has_other_sort = False
+        for p in params:
+            if p.startswith('sort') and p != param:
+                has_other_sort = True
+                break
+        
+        params[param] = 'asc' if has_other_sort else 'desc'
+
+    if delete_others:
+        params = { k: v for k, v in params.items() if not k.startswith('sort') or k == param }
         
     return urlencode(params)
 
