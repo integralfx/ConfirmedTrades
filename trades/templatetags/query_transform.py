5cc47be96a2eff21efdd4fc8b978dcaf9da0181f
integralfx <jordansu83@hotmail.com>
2019-12-22 22:28:31 +1100

    Fix QOL sorting improvement

--- trades/templatetags/query_transform.py
+++ trades/templatetags/query_transform.py
@@ -22,14 +22,8 @@ def sort_query(request, param, delete_others):
         # Initially, the home page is already sorted by the user but doesn't have the 'sort-user=asc' query. Clicking on
         # it in this case, should sort the users in descending order.
         # However, changing the sorted column should sort in ascending order. For example, say the get query is 
-        # 'sort-trades=asc'. Clicking on the user column would result in 'sort-user=asc'.
-        has_other_sort = False
-        for p in params:
-            if p.startswith('sort') and p != param:
-                has_other_sort = True
-                break
-        
-        params[param] = 'asc' if has_other_sort else 'desc'
+        # 'sort-trades=asc'. Clicking on the user column would result in 'sort-user=asc'.        
+        params[param] = 'desc' if param == 'sort-user' and 'sort-user' not in params else 'asc'
 
     if delete_others:
         params = { k: v for k, v in params.items() if not k.startswith('sort') or k == param }
